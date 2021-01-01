8d33aabe6a16691be06a8b59ae12a55e7730bd51
aaaaargZombies <you@example.com>
2021-01-01 16:02:37 +0000

    make responsive, fix status spacing, add rudementary pause button

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let circ = document.getElementsByClassName("progress-ring__circle")[0];
+let timerBtn = document.getElementsByClassName("timer__btn")[0];
 
 const setProgress = (el, percent = 0) => {
 	el.style.strokeDasharray = 1;
 	el.style.strokeDashoffset = 1 - percent;
 };
 
+const startPause = () => {
+	circ.style.animationPlayState === "paused"
+		? (circ.style.animationPlayState = "")
+		: (circ.style.animationPlayState = "paused");
+};
+
 // setProgress(circ, (17 * 60 + 59) / (25 * 60));
+
+circ.addEventListener("animationend", () => {
+	console.log("💥BOOOOM!💥");
+});
+
+// TODO
+timerBtn.onclick = startPause;
+circ.onclick = startPause;
